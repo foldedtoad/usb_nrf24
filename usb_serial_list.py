258b39e70116338565db98dd24231773fa2fc8aa
foldedtoad <foldedtoad@gmail.com>
2023-07-24 16:57:59 -0700

    remove file

--- usb_serial_list.py
+++
@@ -1,8 +0,0 @@
-import serial
-from serial.tools import list_ports
-
-enmu_ports = enumerate(list_ports.comports())
-port = ""
-
-for n, (p, descriptor, hid) in enmu_ports:
-    print(p, descriptor, hid)
